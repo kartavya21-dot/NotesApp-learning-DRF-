535238e16a402f5a97e1154e85583fdbd3afca14
kartavya21-dot <kartavyasharma0987@gmail.com>
2025-10-01 12:24:26 +0530

    Skip collectstatic for deployment

--- Backend/notesapp/build.sh
+++ Backend/notesapp/build.sh
@@ -2,7 +2,9 @@
 # exit on error
 set -o errexit
 
+# Install dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+# Run migrations
 python manage.py migrate
