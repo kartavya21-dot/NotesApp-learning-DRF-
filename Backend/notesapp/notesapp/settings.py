1cf449776fbc091b46a03dd3476f972502ceae9c
kartavya21-dot <kartavyasharma0987@gmail.com>
2025-10-01 11:22:23 +0530

    Trying to host backend, added hosts

--- Backend/notesapp/notesapp/settings.py
+++ Backend/notesapp/notesapp/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://notesapp-m5qb.onrender.com", "localhost"]
 
 # REMOVE THESE TWO LINES - They conflict with Cloudinary!
 # MEDIA_URL = '/media/'
